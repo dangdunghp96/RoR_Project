5c69ffd8ed3caaec13f3471c38a7672afd320aab
Tung Lam <coldwind1911@gmail.com>
2017-12-27 10:19:02 +0700

    Manager categories + providers + fix css history order

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -1,4 +1,14 @@
 class Admin::CategoriesController < Admin::AdminController
   def create
+    category = Category.new category_params
+    if category.save
+      flash[:success] = "Added!"
+      redirect_to admin_products_path
+    end
+  end
+
+  private
+  def category_params
+    params.require(:category).permit(:name,:cate_type)
   end
 end
--- app/controllers/admin/providers_controller.rb
+++ app/controllers/admin/providers_controller.rb
@@ -1,4 +1,14 @@
 class Admin::ProvidersController < Admin::AdminController
   def create
+    provider = Provider.new provider_params
+    if provider.save
+      flash[:success] = "Added!"
+      redirect_to admin_products_path
+    end
+  end
+
+  private
+  def provider_params
+    params.require(:provider).permit(:name,:description)
   end
 end
