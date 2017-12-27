91dfd7e8087865c9c3472d029f0ca010ebc7818f
dangdunghp96 <dangdunghp59@gmail.com>
2017-12-27 09:50:07 +0700

    auth admin

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -1,4 +1,12 @@
 class Admin::OrdersController < Admin::AdminController
   def index
+    @orders = Order.get_pending_orders.paginate(:page => params[:page], :per_page => 10)
+  end
+
+  def complete
+    order = Order.find params[:id]
+    order.complete
+    flash[:success] = "Competed!"
+    redirect_to admin_orders_path
   end
 end
